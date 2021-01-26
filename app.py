752f3ec67895a70cc2c91bfe975f2c97b7fb0528
Chad Schadewald <ces83@students.uwf.edu>
2021-01-26 16:13:46 -0600

    Making routes

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Charles Stephen',
+        'title': 'Blog Post 1',
+        'content': 'First post',
+        'date_posted': '20-Jan-2021'
+    },
+    {
+        'author': 'Stephen Charles',
+        'title': 'Blog Post 2',
+        'content': 'Second post',
+        'date_posted': '20-Jan-2021'
+    }
+]
+
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
