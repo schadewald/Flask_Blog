d53957c9ab135ec76c50277d632152792dd08bd1
Chad Schadewald <ces83@students.uwf.edu>
2021-01-28 14:43:55 -0600

    Working on forms

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0960922431deb454f72e762abc4b5986'
 
 posts = [
     {
@@ -25,3 +28,15 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Register', form=form)
